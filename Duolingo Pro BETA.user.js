0fad9a746969ff4acd5e89c4dbd0ea1a3b93924b
anonymoushackerIV <142535408+anonymoushackerIV@users.noreply.github.com>
2024-03-17 17:28:41 -0400

    Update Duolingo Pro BETA.user.js

--- Duolingo Pro BETA.user.js
+++ Duolingo Pro BETA.user.js
@@ -1,18 +1,5 @@
-// ==UserScript==
-// @name         Duolingo Pro BETA
-// @namespace    Violentmonkey Scripts
-// @version      2.0-BETA-9.6.3
-// @description  Duolingo Auto Solver Tool - Working March 2024
-// @author       anonymoushackerIV
-// @match        https://*.duolingo.com/*
-// @grant        none
-// @license      MIT
-// @require      https://unpkg.com/@supabase/supabase-js@2.12.1
-// @icon         https://github.com/anonymoushackerIV/Duolingo-Pro-Assets/blob/main/images/Duolingo-Pro-Icon.png?raw=true
-// ==/UserScript==
-
 //MIT License
-//Copyright (c) 2023 anonymoushackerIV
+//Copyright (c) 2024 anonymoushackerIV
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
