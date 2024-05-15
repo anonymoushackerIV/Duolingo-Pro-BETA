53cc73b44d7a3a7af016c671ccc490f2ef12e489
anonymoushackerIV <142535408+anonymoushackerIV@users.noreply.github.com>
2024-05-15 16:10:07 -0400

    Update Duolingo Pro BETA.user.js

--- Duolingo Pro BETA.user.js
+++ Duolingo Pro BETA.user.js
@@ -5114,7 +5114,7 @@ function OMEGA() {
             }
 
         } else if (challengeType === 'Pairs') {
-			let nl = document.querySelectorAll('[data-test$="challenge-tap-token"]');
+			let nl = document.querySelectorAll('[data-test*="challenge-tap-token"]:not(span)');
             if (document.querySelectorAll('[data-test="challenge-tap-token-text"]').length === nl.length) {
                 window.sol.pairs?.forEach((pair) => {
                     for (let i = 0; i < nl.length; i++) {
