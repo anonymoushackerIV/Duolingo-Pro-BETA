59e58544f3c1bccb5c860e5f71fb827393bd2705
anonymoushackerIV <142535408+anonymoushackerIV@users.noreply.github.com>
2024-05-15 14:35:46 -0400

    Update Duolingo Pro BETA.user.js

--- Duolingo Pro BETA.user.js
+++ Duolingo Pro BETA.user.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name         Duolingo Pro BETA
-// @namespace    Violentmonkey Scripts
-// @version      2.0-BETA-9.6.8
-// @description  Duolingo Auto Solver Tool - Working May 2024
-// @author       anonymoushackerIV (https://github.com/anonymoushackerIV)
-// @match        https://*.duolingo.com/*
-// @grant        none
-// @license      MIT
-// @icon         https://github.com/anonymoushackerIV/Duolingo-Pro-Assets/blob/main/images/Duolingo-Pro-Icon.png?raw=true
-// ==/UserScript==
-
 // MIT License
 // Copyright (c) 2023 anonymoushackerIV (https://github.com/anonymoushackerIV)
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
